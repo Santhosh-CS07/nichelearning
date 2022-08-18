64bf42be0c59cbbe45a73550c60023571629d402
santhosh-dev07 <santhosh.m07@gmail.com>
2022-08-19 02:28:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from "axios";
+
 import './App.css';
 
 function App() {
+  const [fname, setFname] = useState('');
+  const [subject, setSubject] = useState('');
+  const [email, setEmail] = useState('');
+  const [number, setNumber] = useState('');
+
+  let data = {
+    fname: fname,
+    subject: subject,
+    email: email,
+    number: number
+  }
+  const addData = (e) => {
+    e.preventDefault();
+    axios.post("https://niche-94e5a-default-rtdb.firebaseio.com", data).then(() => alert("Data is submitted"));
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        AWARE THECHNOLOGIES, PURPOSE, AND PLAN TO ACHIEVE BIG THINGS AS A SOFTWARE ENGINEER
       </header>
+      <div>
+        <h2> Let's Learn what skills required for Software Jobs for <span className='free'>FREE</span></h2>
+      </div>
+      <div className='image'>
+        <img src='NicheLearning.png' alt="NicheLearning" height="500px" width="500px" />
+        <form>
+          <div className="title">
+            <i className="fas fa-pencil-alt"></i>
+            <h2>Register here for<span className='free'> FREE 1 WEEK LIVE CLASSES</span></h2>
+          </div>
+          <div className="info">
+            <input className="fname"
+              id="firstName"
+              type="text"
+              placeholder="Full Name"
+              value={fname}
+              onChange={(e) => { setFname(e.target.value) }}
+            />
+            <input
+              id="email"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => { setEmail(e.target.value) }}
+            />
+            <input
+              id="number"
+              type="number"
+              placeholder="Moble Number"
+              value={number}
+              onChange={(e) => { setNumber(e.target.value) }}
+            />
+            <input
+              id="subject"
+              type="text"
+              placeholder="Message"
+              value={subject}
+              onChange={(e) => { setSubject(e.target.value) }}
+            />
+          </div>
+          <button
+            type="button"
+            onClick={addData}
+          >Submit Here</button>
+        </form>
+      </div>
+      <div>
+        <div className="image1">
+          <div>
+            <div className='week_plan'> 1 WEEK <span className='free'>FREE</span> LIVE CLASSES - PLAN</div>
+            <div>
+              <ul>
+                <li>Find Your Problem</li>
+                <li>Find The Technology</li>
+                <li>Prepare a Best PLAN</li>
+                <li>Start Learning Programming Language</li>
+                <li>Create a Project Using Library</li>
+                <li>Practice Interview on Live with me</li>
+                <li>Know the IT Secrets to get the call and Job</li>
+                <li>Web Development | C/C++ & DSA | More</li>
+              </ul>
+            </div>
+          </div>
+          <img className='img1' src='NL_Poster_01.png' alt="NicheLearning" height="500px" width="500px" />
+        </div>
+        <header className="final">
+          THIS WEEK WILL CHANGE YOUR LIFE for <span className='free'>100%</span>
+        </header>
+      </div>
+      <footer>
+        &#169;2022 | created by <a href='www.nichelearning.com'>nichelearning.com</a>
+      </footer>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.EXT_PUBLIC_FIREBASE_APP_MID
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
